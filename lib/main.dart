edf0d7dfb6fdd63e94c41d8afeee31b0d878525a
Bashir <80569644+BashirAbu@users.noreply.github.com>
2022-12-30 21:43:08 +0800

    testing

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'login_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-
+//Testing branch stuff
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
